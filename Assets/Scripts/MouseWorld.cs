95d68ff80da496d8a57d04c9ae7760958aedfa9a
Jess <drengrdev@gmail.com>
2025-08-28 12:53:36 -0500

    Created mouse position with raycast and material to follow it

---
+++ Assets/Scripts/MouseWorld.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseWorld : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Debug.Log(Physics.Raycast(ray));
+    }
+}
