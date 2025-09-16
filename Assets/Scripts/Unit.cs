d4af610b9801f3db866bb2e31b9b2955eab6814b
Jess Salas <drengrdev@gmail.com>
2025-09-16 12:14:37 -0500

    Added walking animation with rotation while moving

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -4,16 +4,28 @@ using UnityEngine;
 
 public class Unit : MonoBehaviour
 {
+    [SerializeField] private Animator unitAnimator;
+
     private Vector3 targetPos;
 
     private void Update()
     {
+
         float stoppingDistance = .1f;
         if(Vector3.Distance(transform.position, targetPos) > stoppingDistance)
         {
             Vector3 moveDir = (targetPos - transform.position).normalized;
             float moveSpeed = 5f;
             transform.position += moveDir * moveSpeed * Time.deltaTime;
+
+            float rotateSpeed = 10f;
+            transform.forward = Vector3.Lerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
+
+            unitAnimator.SetBool("IsWalking", true);
+        }
+        else
+        {
+            unitAnimator.SetBool("IsWalking", false);
         }
 
         if (Input.GetMouseButtonDown(0))
