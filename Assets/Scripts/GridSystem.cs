568eed42ad119dcf76655feaaa6f97a6d0b90372
Jess Salas <drengrdev@gmail.com>
2025-09-19 16:40:46 -0500

    Creating the foundation for the grid system

---
+++ Assets/Scripts/GridSystem.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridSystem
+{
+    private int width;
+    private int height;
+    private float cellSize;
+
+    //Constructor creating the grid system
+    public GridSystem(int width, int height, float cellSize)
+    {
+        this.width = width;
+        this.height = height;
+        this.cellSize = cellSize;
+
+        for(int x = 0; x < width; x++)
+        {
+            for (int z = 0; z < height; z++)
+            {
+                Debug.DrawLine(GetWorldPosition(x, z), GetWorldPosition(x, z) + Vector3.right * .2f, Color.white, 1000);
+                
+            }
+        }
+    }
+
+    //Grabs world position
+    public Vector3 GetWorldPosition(int x, int z)
+    {
+        return new Vector3(x, 0, z) * cellSize;
+    }
+
+    //Converts world position into grid position utilizing the GridPosition struct
+    public GridPosition GetGridPosition(Vector3 worldPosition)
+    {
+        return new GridPosition(
+            Mathf.RoundToInt(worldPosition.x / cellSize),
+            Mathf.RoundToInt(worldPosition.z / cellSize)
+        );
+    }
+}
