d2a92aea29967b7c09b32100d4eb1423d79ca71b
Jess Salas <drengrdev@gmail.com>
2025-10-14 16:18:46 -0500

    Updated project to non security threat editor version. Created onMoveAction delegate for the UnitActionSystem

--- Assets/Scripts/Actions/MoveAction.cs
+++ Assets/Scripts/Actions/MoveAction.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -37,14 +38,16 @@ public class MoveAction : BaseAction
         {
             unitAnimator.SetBool("IsWalking", false);
             isActive = false;
+            onActionComplete();
         }
 
         float rotateSpeed = 10f;
         transform.forward = Vector3.Lerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
     }
 
-    public void Move(GridPosition gridPosition)
+    public void Move(GridPosition gridPosition, Action onActionComplete)
     {
+        this.onActionComplete = onActionComplete;
         this.targetPos = LevelGrid.Instance.GetWorldPosition(gridPosition);
         isActive = true;
     }
