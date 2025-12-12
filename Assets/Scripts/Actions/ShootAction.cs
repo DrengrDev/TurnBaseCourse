66f3ffa108da6aa1ca6f01efb91f4cc0a416c48f
Jess Salas <drengrdev@gmail.com>
2025-12-12 16:03:35 -0600

    Created different grid visual materials depending on each action selected. Grid visual now also shows the range of shooting action.

--- Assets/Scripts/Actions/ShootAction.cs
+++ Assets/Scripts/Actions/ShootAction.cs
@@ -21,7 +21,7 @@ public class ShootAction : BaseAction
     }
 
     private State state;
-    private int maxShootDistance = 7;
+    private int maxShootDistance = 4;
     private float stateTimer;
     private Unit targetUnit;
     private bool canShootBullet;
@@ -159,5 +159,10 @@ public class ShootAction : BaseAction
     public Unit GetTargetUnit()
     {
         return targetUnit;
+    }
+
+    public int GetMaxShootDistance()
+    {
+        return maxShootDistance;
     }
 }
